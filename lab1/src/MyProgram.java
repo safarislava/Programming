935f704f1b2dd032dbd3f362d06f66b01f0075d6
Вячеслав Софьин <safarislava@MacBook-Air-Vaceslav.local>
2024-09-23 09:12:58 +0300

    Fix syntax

--- lab1/src/MyProgram.java
+++ lab1/src/MyProgram.java
@@ -1,22 +1,22 @@
 import java.lang.Math;
 
 public class MyProgram {
-    public static float CalculateCell(short[] n, float[] x, int i, int j) {
+    public static float calculateCell(short[] n, float[] x, int i, int j) {
         if (n[i] == 13) {
             return (float) Math.cos(Math.pow(Math.pow(x[j] / 2, x[j]) / Math.PI, 3));
         }
         else if ((n[i] >= 5 && n[i] <= 10) || n[i] == 14) {
-            float a = (float) Math.pow(Math.E, Math.pow(2.0f/3 * x[j], 3));
+            float a = (float) Math.pow(Math.E, Math.pow(2.0f / 3 * x[j], 3));
             return (float) Math.log(Math.pow(Math.cos(a), 2));
         }
         else {
             float a = (float) Math.pow(4 / Math.tan(x[j]), 2), 
-                b = (float) Math.pow(Math.sin(x[j]), Math.pow(2.0f/3 * x[j], x[j]) / 3) - 1.0f/2;
+                b = (float) Math.pow(Math.sin(x[j]), Math.pow(2.0f / 3 * x[j], x[j]) / 3) - 1.0f / 2;
             return (float) Math.cos(Math.pow(a / b, 3));
         }
     }
 
-    public static void PrintMatrix(float[][] w, int width, int height) {
+    public static void printMatrix(float[][] w, int width, int height) {
         for (int j = 0; j < height; j++) {
             for (int i = 0; i < width; i++) {
                 System.out.printf("%10.5f", w[j][i]);
@@ -43,10 +43,10 @@ public class MyProgram {
         float[][] w = new float[height][width];
         for (int j = 0; j < height; j++) {
             for (int i = 0; i < width; i++) {
-                w[j][i] = CalculateCell(n, x, i, j);
+                w[j][i] = calculateCell(n, x, i, j);
             }        
         }
 
-        PrintMatrix(w, width, height);
+        printMatrix(w, width, height);
     }
 }
